d7b1565343cbd61ab2206e8059f2944616a0c23e
yxj0312 <yanxiajun@hotmail.com>
2020-08-12 16:03:42 +0200

    Install axios and vue router packages

---
+++ routes.js
@@ -0,0 +1,12 @@
+import VideoPlayer from './components/VideoPlayer'
+
+export default {
+    mode: 'history',
+
+    routes: [
+        {
+            path: '/challenge-5',
+            component: VideoPlayer
+        },
+    ]
+}
