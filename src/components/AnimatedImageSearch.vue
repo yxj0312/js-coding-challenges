9c51e71614f94c4d894170666e5f7461f17505ce
yxj0312 <yanxiajun@hotmail.com>
2020-08-13 17:15:07 +0200

    Add search function

--- src/components/AnimatedImageSearch.vue
+++ src/components/AnimatedImageSearch.vue
@@ -1,8 +1,22 @@
 <template>
-    <div>123</div>
+    <div>
+        <form action="">
+            <input 
+                type="text"
+                class="input"
+                placeholder="What images would you like to see on Pixabay?"
+                v-model="query">
+            <button 
+                @click="search"
+            >{{ isSearching ? 'Searching' : 'Search' }}
+            </button>
+        </form>
+    </div>
 </template>
 
 <script>
+import axios from 'axios';
+
     export default {
         props: [],
 
@@ -10,7 +24,11 @@
 
         data() {
             return {
-                
+                apiUrl: 'https://pixabay.com/api',
+                apiKey: '8653965-67fc8570b61c58e735d9adade',
+                images: null,
+                isSearching: false,
+                query: ''
             }
         },
 
@@ -19,7 +37,23 @@
         },
 
         methods: {
-            
+            search() {
+                if (this.query) {
+                    this.images = [];
+                    this.isSearching = true;
+                    const searchQuery = encodeURIComponent(this.query);
+
+                    axios.get(`${this.apiUrl}/?key=${this.apiKey}&q=${searchQuery}s&image_type=photo&per_page=15&safesearch=true`)
+                        .then(res => {
+                            if (res.data.total != 0) {
+                                this.images = res.data.hits
+                                this.isSearching = false;
+                            } else {
+                               this.isSearching = false; 
+                            }
+                        })
+                }
+            }
         }
     }
 </script>
