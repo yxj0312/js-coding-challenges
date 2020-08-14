cabef226fcf4bdfab0e5ebc66ff5d325dabecfe3
yxj0312 <yanxiajun@hotmail.com>
2020-08-14 15:05:26 +0200

    Add transition

--- src/components/AnimatedImageSearch.vue
+++ src/components/AnimatedImageSearch.vue
@@ -1,17 +1,20 @@
 <template>
     <div class="page">
-        <form @submit.prevent="search" class="search-form">
-            <input 
-                type="text"
-                class="input"
-                placeholder="What images would you like to see on Pixabay?"
-                v-model="query">
-            <button
-                type="submit"
-            >{{ isSearching ? 'Searching' : 'Search' }}
-            </button>
-        </form>
-        <div class="hero">
+        <div class="search-area" :class="{'search-area--has-searched':images && images.length !== 0}">
+            <form @submit.prevent="search" class="search-form">
+                <input 
+                    type="text"
+                    class="input"
+                    placeholder="What images would you like to see on Pixabay?"
+                    v-model="query">
+                <button
+                    type="submit"
+                >{{ isSearching ? 'Searching' : 'Search' }}
+                </button>
+            </form>
+            <h3 v-if="images && !images.length && !isSearching" class="not-found-info">No results found!</h3>
+        </div>
+        <div class="hero-area" :class="{'hero-area--has-no-searched':images && images.length === 0}">
             <div v-if="images" class="hero__container">
                 <div v-for="(image, index) in images" :key="`image ${index}`" class="hero__image">
                     <img :src="image.previewURL" alt="">
@@ -72,12 +75,38 @@ import axios from 'axios';
 .page {
     height: 100%;
     height: 100vh;
-    background:burlywood;
+    flex-direction: column;
+    overflow: hidden;
+}
+
+.search-area {
+    height: 100%;
+    background: black;
+    color:cornsilk;
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
-    overflow: hidden;
+    transition: 1s cubic-bezier(.7,.28,.47,1.15) height; 
+}
+
+.search-area--has-searched {
+    height: 50%;
+}
+
+.not-found-info {
+    color:crimson;
+    font-size: 0.875rem;
+}
+
+.hero-area {
+    height:50%;
+    background: cadetblue;
+    transition: 1s cubic-bezier(.7,.28,.47,1.15) height; 
+}
+
+.hero-area--has-no-searched {
+    display: none;
 }
 
 .hero__container {
@@ -85,7 +114,6 @@ import axios from 'axios';
     flex-wrap: wrap;
     justify-content: flex-start;
     align-items: flex-start;
-    margin-top: 72px;
     padding: 0 165px;
 }
 
